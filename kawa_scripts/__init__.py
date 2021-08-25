5e8051222435a7c6d0e43e6c6815e154556faa36
kawashirov <1196609+kawashirov@users.noreply.github.com>
2021-08-25 19:02:08 +0700

    Move UI to submodule.

--- kawa_scripts/__init__.py
+++ kawa_scripts/__init__.py
@@ -33,7 +33,7 @@ if _typing.TYPE_CHECKING:
 	from types import ModuleType
 	from typing import Dict
 	# Эти итак заимпортированы через __import__ но PyCharm их не видит
-	from . import shapekeys, commons, modifiers, vertex_groups
+	from . import _ui
 
 bl_info = {
 	"name": "Kawashirov's Scripts",
@@ -60,6 +60,7 @@ if "bpy" in locals() and "_modules_loaded" in locals():
 
 _modules = [
 	"_internals",
+	"_ui",
 	"atlas_baker",
 	"combiner",
 	"commons",
@@ -86,95 +87,6 @@ del _namespace
 from ._internals import log
 
 
-def _shape_key_edit_mode_context_menu(self, context):
-	self.layout.separator()  # EDIT-mode select
-	self.layout.operator(shapekeys.OperatorSelectVerticesAffectedByShapeKey.bl_idname, icon='VERTEXSEL')
-	self.layout.separator()  # EDIT-mode revert
-	self.layout.operator(shapekeys.OperatorRevertSelectedInActiveToBasis.bl_idname, icon='KEY_DEHLT')
-	self.layout.operator(shapekeys.OperatorRevertSelectedInAllToBasis.bl_idname, icon='KEY_DEHLT')
-	self.layout.separator()  # EDIT-mode apply
-	self.layout.operator(shapekeys.OperatorApplySelectedInActiveToBasis.bl_idname, icon='KEYINGSET')
-	self.layout.operator(shapekeys.OperatorApplySelectedInActiveToAll.bl_idname, icon='KEYINGSET')
-
-
-def _shape_key_object_mode_context_menu(self, context):
-	self.layout.separator()  # OBJECT-mode apply
-	self.layout.operator(shapekeys.OperatorApplyActiveToBasis.bl_idname, icon='KEYINGSET')
-	self.layout.operator(shapekeys.OperatorApplyActiveToAll.bl_idname, icon='KEYINGSET')
-	self.layout.separator()  # OBJECT-mode clean
-	self.layout.operator(shapekeys.OperatorCleanupActive.bl_idname, icon='KEY_DEHLT')
-	self.layout.operator(shapekeys.OperatorCleanupAll.bl_idname, icon='KEY_DEHLT')
-	self.layout.operator(shapekeys.OperatorRemoveEmpty.bl_idname, icon='KEY_DEHLT')
-
-
-# # #
-
-
-class _MESH_MT_shape_key_context_kawa_sub_menu(bpy.types.Menu):
-	bl_label = "Kawashirov"
-	bl_idname = "MESH_MT_shape_key_context_kawa_sub_menu"
-
-	def draw(self, context):
-		_shape_key_edit_mode_context_menu(self, context)
-		_shape_key_object_mode_context_menu(self, context)
-
-
-def _MESH_MT_shape_key_context_menu(self, context):
-	self.layout.menu(_MESH_MT_shape_key_context_kawa_sub_menu.bl_idname)
-
-
-# # #
-
-
-def _MESH_MT_vertex_group_context_menu(self, context):
-	self.layout.separator()
-	self.layout.operator(vertex_groups.OperatorRemoveEmpty.bl_idname, icon='X')
-
-
-# # #
-
-
-class _VIEW3D_MT_object_kawa_sub_menu(bpy.types.Menu):
-	bl_label = "Kawashirov"
-	bl_idname = "VIEW3D_MT_object_kawa_sub_menu"
-
-	def draw(self, context):
-		self.layout.separator()  # transforms
-		self.layout.operator(commons.KawaApplyParentInverseMatrices.bl_idname, icon='ORIENTATION_LOCAL')
-		self.layout.separator()  # modifiers
-		self.layout.operator(modifiers.KawaApplyDeformModifierHighPrecision.bl_idname, icon='MODIFIER')
-		self.layout.operator(modifiers.KawaApplyAllModifiersHighPrecision.bl_idname, icon='MODIFIER')
-		self.layout.operator(modifiers.KawaApplyArmatureToMeshesHighPrecision.bl_idname, icon='ARMATURE_DATA')
-		_shape_key_object_mode_context_menu(self, context)
-		self.layout.separator()  # vertex groups
-		self.layout.operator(vertex_groups.OperatorRemoveEmpty.bl_idname, icon='X')
-
-
-def _VIEW3D_MT_object(self, context):
-	self.layout.menu(_VIEW3D_MT_object_kawa_sub_menu.bl_idname)
-
-
-# # #
-
-class _VIEW3D_MT_edit_mesh_kawa_sub_menu(bpy.types.Menu):
-	bl_label = "Kawashirov"
-	bl_idname = "VIEW3D_MT_edit_mesh_kawa_sub_menu"
-	
-	def draw(self, context):
-		_shape_key_edit_mode_context_menu(self, context)
-
-
-def _VIEW3D_MT_edit_mesh_vertices(self, context):
-	self.layout.menu(_VIEW3D_MT_edit_mesh_kawa_sub_menu.bl_idname)
-
-
-def _VIEW3D_MT_edit_mesh_context_menu(self, context):
-	self.layout.menu(_VIEW3D_MT_edit_mesh_kawa_sub_menu.bl_idname)
-
-
-# # #
-
-
 def register():
 	print("Hello from {0}!".format(__name__))
 	import logging
@@ -193,39 +105,25 @@ def register():
 	
 	from bpy.utils import register_class
 	for mod in _modules_loaded.values():
+		if mod is _ui:
+			continue
 		if mod and hasattr(mod, 'classes'):
 			for cls in mod.classes:
 				register_class(cls)
 	
-	bpy.utils.register_class(_MESH_MT_shape_key_context_kawa_sub_menu)
-	bpy.utils.register_class(_VIEW3D_MT_object_kawa_sub_menu)
-	bpy.utils.register_class(_VIEW3D_MT_edit_mesh_kawa_sub_menu)
-	
-	bpy.types.VIEW3D_MT_object.append(_VIEW3D_MT_object)
-	bpy.types.VIEW3D_MT_object_context_menu.append(_VIEW3D_MT_object)
-	bpy.types.VIEW3D_MT_edit_mesh_context_menu.append(_VIEW3D_MT_edit_mesh_context_menu)
-	bpy.types.VIEW3D_MT_edit_mesh_vertices.append(_VIEW3D_MT_edit_mesh_vertices)
-	bpy.types.MESH_MT_shape_key_context_menu.append(_MESH_MT_shape_key_context_menu)
-	bpy.types.MESH_MT_vertex_group_context_menu.append(_MESH_MT_vertex_group_context_menu)
+	_ui.register()
 	
 	log.info("Hello from {0} once again!".format(__name__))
 
 
 def unregister():
 	from bpy.utils import unregister_class
-
-	bpy.types.VIEW3D_MT_object.remove(_VIEW3D_MT_object)
-	bpy.types.VIEW3D_MT_object_context_menu.remove(_VIEW3D_MT_object)
-	bpy.types.VIEW3D_MT_edit_mesh_context_menu.remove(_VIEW3D_MT_edit_mesh_context_menu)
-	bpy.types.VIEW3D_MT_edit_mesh_vertices.remove(_VIEW3D_MT_edit_mesh_vertices)
-	bpy.types.MESH_MT_shape_key_context_menu.remove(_MESH_MT_shape_key_context_menu)
-	bpy.types.MESH_MT_vertex_group_context_menu.remove(_MESH_MT_vertex_group_context_menu)
 	
-	bpy.utils.unregister_class(_MESH_MT_shape_key_context_kawa_sub_menu)
-	bpy.utils.unregister_class(_VIEW3D_MT_object_kawa_sub_menu)
-	bpy.utils.unregister_class(_VIEW3D_MT_edit_mesh_kawa_sub_menu)
+	_ui.unregister()
 	
 	for mod in reversed(_modules_loaded.values()):
+		if mod is _ui:
+			continue
 		if hasattr(mod, 'classes'):
 			for cls in reversed(mod.classes):
 				if cls.is_registered:
---
+++ kawa_scripts/_ui.py
@@ -0,0 +1,129 @@
+# Kawashirov's Scripts (c) 2021 by Sergey V. Kawashirov
+#
+# Kawashirov's Scripts is licensed under a
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#
+# You should have received a copy of the license along with this
+# work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
+#
+#
+import bpy as _bpy
+
+from . import commons as _commons
+from . import shapekeys as _shapekeys
+from . import vertex_groups as _vertex_groups
+from . import modifiers as _modifiers
+
+
+def _shape_key_edit_mode_context_menu(self, context):
+	self.layout.separator()  # EDIT-mode select
+	self.layout.operator(_shapekeys.OperatorSelectVerticesAffectedByShapeKey.bl_idname, icon='VERTEXSEL')
+	self.layout.separator()  # EDIT-mode revert
+	self.layout.operator(_shapekeys.OperatorRevertSelectedInActiveToBasis.bl_idname, icon='KEY_DEHLT')
+	self.layout.operator(_shapekeys.OperatorRevertSelectedInAllToBasis.bl_idname, icon='KEY_DEHLT')
+	self.layout.separator()  # EDIT-mode apply
+	self.layout.operator(_shapekeys.OperatorApplySelectedInActiveToBasis.bl_idname, icon='KEYINGSET')
+	self.layout.operator(_shapekeys.OperatorApplySelectedInActiveToAll.bl_idname, icon='KEYINGSET')
+
+
+def _shape_key_object_mode_context_menu(self, context):
+	self.layout.separator()  # OBJECT-mode apply
+	self.layout.operator(_shapekeys.OperatorApplyActiveToBasis.bl_idname, icon='KEYINGSET')
+	self.layout.operator(_shapekeys.OperatorApplyActiveToAll.bl_idname, icon='KEYINGSET')
+	self.layout.separator()  # OBJECT-mode clean
+	self.layout.operator(_shapekeys.OperatorCleanupActive.bl_idname, icon='KEY_DEHLT')
+	self.layout.operator(_shapekeys.OperatorCleanupAll.bl_idname, icon='KEY_DEHLT')
+	self.layout.operator(_shapekeys.OperatorRemoveEmpty.bl_idname, icon='KEY_DEHLT')
+
+
+# # #
+
+
+class _MESH_MT_shape_key_context_kawa_sub_menu(_bpy.types.Menu):
+	bl_label = "Kawashirov"
+	bl_idname = "MESH_MT_shape_key_context_kawa_sub_menu"
+	
+	def draw(self, context):
+		_shape_key_edit_mode_context_menu(self, context)
+		_shape_key_object_mode_context_menu(self, context)
+
+
+def _MESH_MT_shape_key_context_menu(self, context):
+	self.layout.menu(_MESH_MT_shape_key_context_kawa_sub_menu.bl_idname)
+
+
+# # #
+
+
+def _MESH_MT_vertex_group_context_menu(self, context):
+	self.layout.separator()
+	self.layout.operator(_vertex_groups.OperatorRemoveEmpty.bl_idname, icon='X')
+
+
+# # #
+
+
+class _VIEW3D_MT_object_kawa_sub_menu(_bpy.types.Menu):
+	bl_label = "Kawashirov"
+	bl_idname = "VIEW3D_MT_object_kawa_sub_menu"
+	
+	def draw(self, context):
+		self.layout.separator()  # transforms
+		self.layout.operator(_commons.KawaApplyParentInverseMatrices.bl_idname, icon='ORIENTATION_LOCAL')
+		self.layout.separator()  # modifiers
+		self.layout.operator(_modifiers.KawaApplyDeformModifierHighPrecision.bl_idname, icon='MODIFIER')
+		self.layout.operator(_modifiers.KawaApplyAllModifiersHighPrecision.bl_idname, icon='MODIFIER')
+		self.layout.operator(_modifiers.KawaApplyArmatureToMeshesHighPrecision.bl_idname, icon='ARMATURE_DATA')
+		_shape_key_object_mode_context_menu(self, context)
+		self.layout.separator()  # vertex groups
+		self.layout.operator(_vertex_groups.OperatorRemoveEmpty.bl_idname, icon='X')
+
+
+def _VIEW3D_MT_object(self, context):
+	self.layout.menu(_VIEW3D_MT_object_kawa_sub_menu.bl_idname)
+
+
+# # #
+
+class _VIEW3D_MT_edit_mesh_kawa_sub_menu(_bpy.types.Menu):
+	bl_label = "Kawashirov"
+	bl_idname = "VIEW3D_MT_edit_mesh_kawa_sub_menu"
+	
+	def draw(self, context):
+		_shape_key_edit_mode_context_menu(self, context)
+
+
+def _VIEW3D_MT_edit_mesh_vertices(self, context):
+	self.layout.menu(_VIEW3D_MT_edit_mesh_kawa_sub_menu.bl_idname)
+
+
+def _VIEW3D_MT_edit_mesh_context_menu(self, context):
+	self.layout.menu(_VIEW3D_MT_edit_mesh_kawa_sub_menu.bl_idname)
+
+
+# # #
+
+def register():
+	_bpy.utils.register_class(_MESH_MT_shape_key_context_kawa_sub_menu)
+	_bpy.utils.register_class(_VIEW3D_MT_object_kawa_sub_menu)
+	_bpy.utils.register_class(_VIEW3D_MT_edit_mesh_kawa_sub_menu)
+	
+	_bpy.types.VIEW3D_MT_object.append(_VIEW3D_MT_object)
+	_bpy.types.VIEW3D_MT_object_context_menu.append(_VIEW3D_MT_object)
+	_bpy.types.VIEW3D_MT_edit_mesh_context_menu.append(_VIEW3D_MT_edit_mesh_context_menu)
+	_bpy.types.VIEW3D_MT_edit_mesh_vertices.append(_VIEW3D_MT_edit_mesh_vertices)
+	_bpy.types.MESH_MT_shape_key_context_menu.append(_MESH_MT_shape_key_context_menu)
+	_bpy.types.MESH_MT_vertex_group_context_menu.append(_MESH_MT_vertex_group_context_menu)
+
+
+def unregister():
+	_bpy.types.VIEW3D_MT_object.remove(_VIEW3D_MT_object)
+	_bpy.types.VIEW3D_MT_object_context_menu.remove(_VIEW3D_MT_object)
+	_bpy.types.VIEW3D_MT_edit_mesh_context_menu.remove(_VIEW3D_MT_edit_mesh_context_menu)
+	_bpy.types.VIEW3D_MT_edit_mesh_vertices.remove(_VIEW3D_MT_edit_mesh_vertices)
+	_bpy.types.MESH_MT_shape_key_context_menu.remove(_MESH_MT_shape_key_context_menu)
+	_bpy.types.MESH_MT_vertex_group_context_menu.remove(_MESH_MT_vertex_group_context_menu)
+	
+	_bpy.utils.unregister_class(_MESH_MT_shape_key_context_kawa_sub_menu)
+	_bpy.utils.unregister_class(_VIEW3D_MT_object_kawa_sub_menu)
+	_bpy.utils.unregister_class(_VIEW3D_MT_edit_mesh_kawa_sub_menu)
