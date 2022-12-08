07f4300d8758ba81eb519979950d0eb10920d7b3
kawashirov <1196609+kawashirov@users.noreply.github.com>
2022-12-08 08:14:41 +0700

    Update Project settings

--- update_docs.cmd
+++ update_docs.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal 
 pushd %~dp0
-set PYTHONPATH=%PYTHONPATH%;%CD%\blender_autocomplete\2.83;%CD%\pdoc
+set PYTHONPATH=%PYTHONPATH%;%CD%\blender_autocomplete\3.0;%CD%\pdoc
 python.exe -m pdoc -f -o .tmp_doc --html kawa_scripts
 del /f /q gh-pages\*.html
 xcopy /s /v /f /h /r .tmp_doc\kawa_scripts\* gh-pages
