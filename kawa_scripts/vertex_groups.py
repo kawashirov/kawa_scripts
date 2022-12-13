9a5309e9a2d5a5a35b1004730f080d68083fbe2c
kawashirov <1196609+kawashirov@users.noreply.github.com>
2022-12-13 11:24:43 +0700

    merge_weights -> class WeightsMerger

--- kawa_scripts/vertex_groups.py
+++ kawa_scripts/vertex_groups.py
@@ -10,25 +10,24 @@
 """
 Useful tools for Vertex Groups
 """
-from math import sqrt as _sqrt
 
-import bpy as _bpy
-import bmesh as _bmesh
+import bpy
 import bpy.app
+from bpy.types import Object, Mesh, VertexGroup, Context, Operator
+
+import bmesh
+from bmesh.types import BMVert, BMLayerItem, BMDeformVert
 
 from . import _internals
 from . import _doc
-from ._internals import log as _log
-from . import objects as _objects
-from . import meshes as _meshes
+from ._internals import log
+from . import objects
+from . import meshes
 
-import typing as _typing
+import typing
 
-if _typing.TYPE_CHECKING:
+if typing.TYPE_CHECKING:
 	from .objects import HandyMultiObject
-	from typing import Union, Tuple, Dict, List, Optional
-	from bpy.types import Object, Mesh, VertexGroup, Context, MeshVertex, Operator
-	from bmesh.types import BMLayerItem, BMDeformVert
 
 
 def _any_weight(mesh: 'Mesh', group_index: 'int', limit: 'float' = 0.0):
@@ -48,233 +47,172 @@ def get_weight_safe(group: 'VertexGroup', index: 'int', default=0.0):
 		return default
 
 
-def halfbone_apply_armature(objs: 'HandyMultiObject'):
-	objs = list(_objects.resolve_objects(objs))
-	armature_obj = list(obj for obj in objs if obj.type == 'ARMATURE' and isinstance(obj.data, _bpy.types.Armature))
-	if len(armature_obj) != 1:
-		raise RuntimeError(f"There is no single Armature-Object provided ({len(armature_obj)})")
-	armature_obj = armature_obj[0]  # type: Union[Object, List[Object]]
-	
-	armature_data = armature_obj.data  # type: _bpy.types.Armature
-	
-	if armature_data.bones.active is None:
-		raise RuntimeError(f"There is no active bone in Armature-Object provided ({armature_obj})")
-	half_name = armature_data.bones.active.name
-	
-	selected = list(bone.name for bone in armature_data.bones if armature_data.bones.active != bone and bone.select)
-	if len(selected) != 2:
-		raise RuntimeError(f"There is no two selected control bones in Armature-Object provided ({len(selected)})")
-	ctrl_a, ctrl_b = selected
-	
-	mesh_objs = list(obj for obj in objs if _meshes.is_mesh_object(obj))
-	if len(mesh_objs) < 1:
-		raise RuntimeError(f"There is no Mesh-Objects provided ({len(mesh_objs)})")
-	
-	halfbone_apply_weight(mesh_objs, ctrl_a, ctrl_b, half_name)
-
-
-def halfbone_apply_weight(objs: '_objects.HandyMultiObject', ctrl_a: 'Union[str, int]', ctrl_b: 'Union[str, int]',
-		half_name: 'Union[str, int]'):
-	for obj in _objects.resolve_objects(objs):
-		halfbone_apply_weight_single(obj, ctrl_a, ctrl_b, half_name)
-
-
-def halfbone_apply_weight_single(obj: 'Object', ctrl_a: 'Union[str, int]', ctrl_b: 'Union[str, int]', half_name: 'Union[str, int]'):
-	mesh = obj.data  # type: Mesh
-	a_group = obj.vertex_groups.get(ctrl_a)
-	b_group = obj.vertex_groups.get(ctrl_b)
-	if a_group is None or b_group is None:
-		return
-	half_group = obj.vertex_groups.get(half_name)
-	if half_group is None:
-		half_group = obj.vertex_groups.new(name=half_name)
-	index_list = [0]
-	for v in mesh.vertices:  # type: MeshVertex
-		a_w = get_weight_safe(a_group, v.index)
-		if 1.0 <= a_w or a_w <= 0.0:
-			continue
-		b_w = get_weight_safe(b_group, v.index)
-		if 1.0 <= a_w or a_w <= 0.0:
-			continue
-		lg = a_w + b_w  # <1
-		a_wn, b_wn = a_w / lg, b_w / lg
-		# вычисляем новый вес для полугурппы
-		h_wn = min(a_wn, b_wn) * 2.0 * lg
-		# отображение [0.5..1] в [0...1]
-		a_wn = min(max(a_wn * 2.0 - 1.0, 0.0), 1.0) * lg
-		b_wn = min(max(b_wn * 2.0 - 1.0, 0.0), 1.0) * lg
-		#
-		print("#{}: {}, {} -> {}, {} and {}".format(v.index, a_w, b_w, a_wn, b_wn, h_wn))
-		index_list[0] = v.index
-		a_group.add(index_list, a_wn, 'REPLACE')
-		b_group.add(index_list, b_wn, 'REPLACE')
-		half_group.add(index_list, h_wn, 'ADD')
-
-
-# def halfbone_apply_weight(obj: 'bpy.types.Object', control_names: 'Iterable[Union[str, int]]', half_name: 'Union[str, int]',
-# 		step=0.5, smoothstep=1.0, amplitide=1.0):
-# 	mesh = obj.data  # type: Mesh
-# 	control_groups = list(obj.vertex_groups.get(n) for n in control_names)
-# 	if any(True for g in control_groups if g is None):
-# 		raise ValueError('control_names')
-#
-# 	control_group = obj.vertex_groups.get(control_name)
-# 	half_group = obj.vertex_groups.get(half_name)
-# 	if half_group is None:
-# 		raise ValueError('half_name')
-# 	step = max(min(step, 1), 0)
-# 	vert_count = 0
-# 	for v in mesh.vertices:  # type: MeshVertex
-# 		control_w = get_weight_safe(control_group, v.index)
-# 		f = 1.0 - abs(control_w * 2.0 - 1.0)  # отображает [0..0.5],[0.5..1] в [0..1],[1..0]
-# 		f = (f - step) / (1.0 - step)  # отображает [0..1] в [step..1]
-# 		if f < 0:
-# 			continue
-# 		smooth_f = f * f * (3.0 - 2.0 * f)  # cubic smoothstep ([0..1] в [0..1])
-# 		f = smooth_f * smoothstep + f * (1 - smoothstep)  # линкомбинация
-# 		half_w = f * amplitide
-# 		index_list = [v.index]
-# 		for other_group in obj.vertex_groups:
-# 			if other_group is half_group:
-# 				continue
-# 			other_w = get_weight_safe(other_group, v.index)
-# 			if other_w <= 0.0:
-# 				continue
-# 			other_w = other_w * (1.0 - half_w)
-# 			other_group.add(index_list, other_w, 'REPLACE')
-# 		half_group.add(index_list, half_w, 'REPLACE')
-# 		vert_count += 1
-# 	return vert_count
-
-
-def merge_weights(objs: 'HandyMultiObject', mapping: 'Dict[str, Dict[str, float]]',
-		strict: 'Optional[bool]' = None) -> 'Tuple[int, int, int]':
+class WeightsMerger:
 	"""
 	Merges vertex groups weights on given Mesh-objects, using given mapping of weights.
 	Mapping should contain names of groups that should be merged as keys and list of tuple-pairs as values.
 	Each pair should contain name of target vertex group and it's weight.
 	Sum of pair's weights may not be 1, but if you want to preserve original sum of weights (keep normalized weights)
 	you must keep pair's weights sum at 1.
-	
+
 	For example, if a vertex on your object have following weights:
-	
+
 	`Apple`: `0.1`, `Orange`: `0.2`, `Grape`: `0.3`, `Banana`: `0.4` (their sum is `1.0`)
-	
+
 	and then you run this function with `mapping` = `{'Banana': {'Apple': 0.75, 'Orange': 0.25)}`
-	
+
 	you will get next weights:
-	
+
 	`Apple`: `0.4`, `Orange`: `0.3`, `Grape`: `0.3` (their sum is also `1.0`)
-	
+
 	75% of `Banana` weight (which is `0.3`) was added to `Apple` and
 	25% of `Banana` weight (which is `0.1`) was added to `Orange` and
 	vertex group `Banana` it self was removed.
-	
+
 	This function can be used to merge one bones of armatures to others,
 	like in CATS, but multiple targets are supported.
-	
+
 	Returns tuple of 3 ints: verts_modified, groups_removed, objects_modified
 	"""
 	
-	verts_modified, groups_removed, meshes_modified = 0, 0, 0
-	new_weights = dict()  # type: Dict[int, float]  # временный
-	remap = list()  # type: List[Union[None, List[float]]]  # также временный
+	def __init__(self, objs: 'HandyMultiObject', mapping: 'dict[str, dict[str, float]]', strict: 'bool|None' = None):
+		self.objs = objs
+		self.mapping = mapping
+		self.strict = strict
+		
+		self.verts_modified = 0
+		self.groups_removed = 0
+		self.objects_iterated = 0
+		self.objects_modified = 0
+		
+		# Используются как локальные переменные между методами
+		self._cur_obj = None  # type: Object|None
+		self._cur_mesh = None  # type: Mesh|None
+		self._int_mapping = None  # type: list[list[tuple[int,float]]|None]|None
+		self._deform_layer = None  # type: BMLayerItem|None
+		# общий временный для избегания новых аллокаций, номер группы -> новый вес
+		self._new_weights = dict()  # type: dict[int, float]
 	
-	# In Blender 3.0+ vertex weight data fully stored in Mesh, not Object, so we can skip repeating meshes.
-	meshes = set() if bpy.app.version[0] >= 3 else None
-	for obj in _objects.resolve_objects(objs):
-		mesh = _meshes.get_safe(obj, strict=strict)
-		if mesh is None:
-			continue
-		if meshes is not None:
-			if mesh in meshes:
-				continue
-			meshes.add(mesh)
-		if not _objects.ensure_in_mode(obj, 'OBJECT', strict=strict):
-			continue
-		if len(obj.vertex_groups) < 1:
-			# Если на меши нет групп вообще, то и объединять нечего.
-			continue
+	def _need_mapping(self, obj: 'Object'):
+		if obj.vertex_groups is None or len(obj.vertex_groups) < 1:
+			return False
+		return any(src_name in obj.vertex_groups for src_name in self.mapping.keys())
+	
+	def _ensure_groups_exist(self):
 		# досоздаём необходимые группы на объекте
-		object_modified = False
-		any_to_merge = False
-		for src_bone, targets in mapping.items():
-			if src_bone in obj.vertex_groups:
-				any_to_merge = True
-				for dst_bone, _ in targets.items():
-					if dst_bone not in obj.vertex_groups:
-						obj.vertex_groups.new(name=dst_bone)
-		if not any_to_merge:
-			# Если на меши нет объединяемых групп, то делать тут тоже нечего.
-			_log.info('There is no groups to merge on {} (any_to_merge).'.format(repr(obj)))
-			continue
+		for src_name, targets in self.mapping.items():
+			if src_name in self._cur_obj.vertex_groups:
+				for dst_name, _ in targets.items():
+					if dst_name not in self._cur_obj.vertex_groups:
+						dst_group = self._cur_obj.vertex_groups.new(name=dst_name)
+						assert dst_group.name == dst_name, f"{dst_group.name=!r}, {dst_name=!r}"
+						msg = f'Created new VertexGroup {dst_group.name!r} on Object {self._cur_obj.name!r}, Mesh {self._cur_mesh.name!r}.'
+						log.info(msg)
+	
+	def _make_int_mapping(self):
 		# создаём отображение на листе в intах что бы быстро
-		for i in range(max(len(obj.vertex_groups), len(remap))):
-			if len(remap) <= i:
-				remap.append(None)  # Дорасширяем лист
-			else:
-				remap[i] = None
+		self._int_mapping = list(None for _ in range(len(self._cur_obj.vertex_groups)))  # type: list[list[tuple[int,float]]|None]
 		# заполняем отображение
-		for src_bone, targets in mapping.items():
-			src_vg = obj.vertex_groups.get(src_bone)
-			if src_vg is None or len(targets) < 1:
+		for src_name, targets in self.mapping.items():
+			src_group = self._cur_obj.vertex_groups.get(src_name)
+			if src_group is None or len(targets) < 1:
 				continue
-			remap[src_vg.index] = list()
-			for dst_bone, weight in targets.items():
-				dst_vg = obj.vertex_groups.get(dst_bone)
-				remap[src_vg.index].append((dst_vg.index, weight))
-		if not any(remap):
-			_log.info('There is no groups to merge on {} (any(remap)).'.format(repr(obj)))
-			continue  # на этом объекте нет групп, которые нужно смешивать
-		_log.info('Remap for {}: {}.'.format(repr(obj), repr(remap)))
-		bm = _bmesh.new()
+			weights = self._int_mapping[src_group.index] = list()  # type: list[tuple[int,float]]
+			for dst_name, weight in targets.items():
+				dst_vg = self._cur_obj.vertex_groups.get(dst_name)
+				assert dst_vg, f"{self._cur_obj!r}, {self._cur_mesh!r}: missing group {dst_name!r}: {dst_vg!r}"
+				weights.append((dst_vg.index, weight))
+		assert len(self._int_mapping) > 0, f"{self._cur_obj!r}, {self._cur_mesh!r}: int-mapping is empty!"
+	
+	def _apply_mapping_vert(self, bm_vert: 'BMVert'):
+		bm_deform_vert = bm_vert[self._deform_layer]  # type: BMDeformVert
+		modified_weights = False
+		self._new_weights.clear()
+		for src_index, src_weight in bm_deform_vert.items():
+			if self._int_mapping[src_index] is None:
+				# вес не сливается: сохраняем как есть
+				self._new_weights[src_index] = src_weight + self._new_weights.get(src_index, 0.0)
+			else:
+				# вес сливается: микшируем
+				for dst_index, dst_weight in self._int_mapping[src_index]:
+					self._new_weights[dst_index] = dst_weight * src_weight + self._new_weights.get(dst_index, 0.0)
+				modified_weights = True
+		if modified_weights:
+			bm_deform_vert.clear()
+			for new_index, new_weight in self._new_weights.items():
+				bm_deform_vert[new_index] = new_weight  # slice not supported
+		# dbg_msg = f'New weights for Object {self._cur_obj.name!r}, Mesh {self._cur_mesh.name!r} on {bm_vert!r}: '
+		# f'{self._new_weights=!r}, {bm_deform_vert=!r}.'
+		# _log.info(dbg_msg)
+		return modified_weights
+	
+	def _apply_mapping(self) -> 'bool':
+		bm = bmesh.new()
 		try:
-			bm.from_mesh(obj.data)
-			deform_layer = bm.verts.layers.deform.active  # type: BMLayerItem
-			if deform_layer is None:
+			bm.from_mesh(self._cur_mesh)
+			self._deform_layer = bm.verts.layers.deform.active  # type: BMLayerItem
+			if self._deform_layer is None:
 				# Такая ситуация случается если на меши есть группы, но ни одна точка не привязана.
-				_log.info('There is no deform layer on {}.'.format(repr(obj)))
-				continue
+				log.info(f'There is no deform_layer on Object {self._cur_obj.name!r}, Mesh {self._cur_mesh.name!r}.')
+				return False
 			bm.verts.ensure_lookup_table()
-			for v in bm.verts:
-				dv = v[deform_layer]  # type: BMDeformVert
-				modified = False
-				new_weights.clear()
-				for src_index, src_weight in dv.items():
-					if remap[src_index] is None:
-						# вес не сливается: сохраняем как есть
-						new_weights[src_index] = src_weight + new_weights.get(src_index, 0.0)
-					else:
-						# вес сливается: микшируем
-						for dst_index, dst_weight in remap[src_index]:
-							new_weights[dst_index] = dst_weight * src_weight + new_weights.get(dst_index, 0.0)
-						modified = True
-				if modified:
-					object_modified = True
-					dv.clear()
-					for new_index, new_weight in new_weights.items():
-						dv[new_index] = new_weight  # slice not supported
-					# _log.info('New weights for {!r} in {!r}: new_weights={!r}, v[deform_layer]={!r}.'.format(obj, v, new_weights, dv))
+			verts_modified = 0
+			for bm_vert in bm.verts:
+				if self._apply_mapping_vert(bm_vert):
 					verts_modified += 1
-			bm.to_mesh(obj.data)
+			log.info(f'Modified {verts_modified} vertices weights on Object {self._cur_obj.name!r}, Mesh {self._cur_mesh.name!r}.')
+			if verts_modified > 0:
+				bm.to_mesh(self._cur_mesh)
+				self.verts_modified += verts_modified
 		finally:
 			if bm is not None:
 				bm.free()
-		for src_name, src_group in obj.vertex_groups.items():
-			if src_name in mapping:
+		return verts_modified > 0
+	
+	def _remove_remapped_groups(self):
+		groups_removed = 0
+		for src_name, src_group in self._cur_obj.vertex_groups.items():
+			if src_name in self.mapping.keys():
 				groups_removed += 1
-				object_modified = True
-				obj.vertex_groups.remove(src_group)
-		if object_modified:
-			meshes_modified += 1
-	if verts_modified == 0 or groups_removed == 0 or meshes_modified == 0:
-		# Если любой равен нулю, то и остальые тоже должны быть равны нулю.
-		assert verts_modified == 0 and groups_removed == 0 and meshes_modified == 0
-	return verts_modified, groups_removed, meshes_modified
+				self._cur_obj.vertex_groups.remove(src_group)
+		# Если мы начали обрабатывать этот объект,
+		# значит была хотя бы одна группа, которая сейчас должна быть удалена.
+		assert groups_removed > 0, f"{self._cur_obj!r}, {self._cur_mesh!r}: {groups_removed=!r}"
+		self.groups_removed += groups_removed
+	
+	def merge_weights(self):
+		# TODO args checks
+		processed_meshes = set() if bpy.app.version[0] >= 3 else None
+		for _obj in objects.resolve_objects(self.objs):
+			self.objects_iterated += 1
+			self._cur_obj = _obj
+			self._cur_mesh = meshes.get_safe(self._cur_obj, strict=self.strict)
+			if self._cur_mesh is None:
+				continue
+			if processed_meshes is not None:
+				if self._cur_mesh in processed_meshes:
+					continue
+				processed_meshes.add(self._cur_mesh)
+			if not objects.ensure_in_mode(self._cur_obj, 'OBJECT', strict=self.strict):
+				continue
+			
+			if not self._need_mapping(self._cur_obj):
+				log.info(f'There is no groups to merge on Object {self._cur_obj.name!r} (Mesh {self._cur_mesh.name!r}).')
+				return False
+			
+			self._ensure_groups_exist()
+			self._make_int_mapping()
+			
+			if not self._apply_mapping():
+				log.info(f'Actual weights values was not changed on Object {self._cur_obj.name!r} (Mesh {self._cur_mesh.name!r}).')
+			
+			self._remove_remapped_groups()
+			self.objects_modified += 1
+		log.info(f"Merged weights on {self.objects_modified}/{self.objects_iterated} objects: {self.verts_modified} vertices changed.")
 
 
 def remove_empty(objs: 'HandyMultiObject', limit: 'float' = 0.0, ignore_locked: 'bool' = False,
-		strict: 'Optional[bool]' = None, op: 'Operator' = None) -> 'Tuple[int, int]':
+		strict: 'bool|None' = None, op: 'Operator' = None) -> 'tuple[int, int]':
 	"""
 	**Removes empty Vertex Groups from given objects.**
 	
@@ -300,16 +238,16 @@ def remove_empty(objs: 'HandyMultiObject', limit: 'float' = 0.0, ignore_locked: 
 	"""
 	removed_groups, removed_objects = 0, 0
 	# In Blender 3.0+ vertex weight data fully stored in Mesh, not Object, so we can skip repeating meshes.
-	meshes = set() if _bpy.app.version[0] >= 3 else None
-	for obj in _objects.resolve_objects(objs):
-		mesh = _meshes.get_safe(obj, strict=strict)
+	processed_meshes = set() if bpy.app.version[0] >= 3 else None
+	for obj in objects.resolve_objects(objs):
+		mesh = meshes.get_safe(obj, strict=strict)
 		if mesh is None:
 			continue
-		if meshes is not None:
-			if mesh in meshes:
+		if processed_meshes is not None:
+			if mesh in processed_meshes:
 				continue
-			meshes.add(mesh)
-		if not _objects.ensure_in_mode(obj, 'OBJECT', strict=strict):
+			processed_meshes.add(mesh)
+		if not objects.ensure_in_mode(obj, 'OBJECT', strict=strict):
 			continue
 		if obj.vertex_groups is None or len(obj.vertex_groups) < 1:
 			continue
@@ -341,7 +279,7 @@ class OperatorRemoveEmpty(_internals.KawaOperator):
 	))
 	bl_options = {'REGISTER', 'UNDO'}
 	
-	limit: _bpy.props.FloatProperty(
+	limit: bpy.props.FloatProperty(
 		name="Limit",
 		description="Do not count vertices which weight is below or equal to this limit. Count everything if less than zero.",
 		min=-1e-05,
@@ -351,7 +289,7 @@ class OperatorRemoveEmpty(_internals.KawaOperator):
 		subtype='FACTOR',
 	)
 	
-	ignore_locked: _bpy.props.BoolProperty(
+	ignore_locked: bpy.props.BoolProperty(
 		name="Ignore Locked",
 		description="Do not remove locked (with flag lock_weight) groups.",
 		default=False,
@@ -372,13 +310,20 @@ class OperatorRemoveEmpty(_internals.KawaOperator):
 	def execute(self, context: 'Context'):
 		removed_groups, removed_objects = remove_empty(self.get_selected_objs(context),
 			limit=self.limit, ignore_locked=self.ignore_locked, op=self)
-		_log.info("Removed {0} vertex groups from {1} objects.".format(removed_groups, removed_objects), op=self)
+		log.info("Removed {0} vertex groups from {1} objects.".format(removed_groups, removed_objects), op=self)
 		return {'FINISHED'} if removed_groups > 0 else {'CANCELLED'}
 
 
 classes = (
 	OperatorRemoveEmpty,
 )
+
+__all__ = [
+	'get_weight_safe',
+	'WeightsMerger',
+	'remove_empty',
+	'OperatorRemoveEmpty'
+]
 
 __pdoc__ = dict()
 _doc.process_blender_classes(__pdoc__, classes)
